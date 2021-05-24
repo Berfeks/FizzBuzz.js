597d73035157dda5e4f64439ec061a605bae1991
Maksym Plevnytskyi <76871115+Berfeks@users.noreply.github.com>
2021-05-24 18:28:16 +0300

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,19 @@
+let number = 15;
+let taskResult;
+
+
+if (number% 5==0 && number% 3==0) {
+  taskResult = 'FizzBuzz'
+  console.log(taskResult = 'FizzBuzz') 
+  }
+ else if (number% 5==0){
+ taskResult = 'Buzz'
+ console.log(taskResult = 'Buzz') 
+}
+else if (number% 3==0){
+  taskResult = 'Fizz'
+   console.log(taskResult = 'Fizz') 
+}
+else{
+ console.log(taskResult = (number));
+}
